012272f7e52c54a9bbfc841bdd791ca7e83f5987
Modi-Naitik <naitikmodi9999@gmail.com>
2024-02-23 18:36:03 +0530

    Done All Functionality

--- public/script.js
+++ public/script.js
@@ -16,9 +16,9 @@ navigator.mediaDevices.getUserMedia({ //we can attemp to access the webcame and 
     video: true,
     audio: true
 }).then(stream => {
-    myvideoStream=stream;
+    myvideoStream = stream;
     addvideostream(myvideo, stream);
-    peer.on('call', call => {   
+    peer.on('call', call => {
         call.answer(stream)
         const video = document.createElement('video');
         call.on('stream', userVideoStream => {
@@ -55,4 +55,84 @@ const addvideostream = (video, stream) => {
         video.play();
     })
     videoGrid.append(video);
+}
+
+let text = $('input')
+
+//Doing Chat
+$('html').keydown((e) => {
+    if (e.which == 13 && text.val().length !== 0) {
+        console.log(text.val())
+        socket.emit('message', text.val());
+        text.val('')
+    }
+})
+
+socket.on('createMessage', message => {
+    $('ul').append(`<li class="message"><b>user</b><br/>${message}</li>`)
+})
+
+//Scroll For Chat
+const scrollToBottom = () => {
+    var d = $('.main__chat_window');
+    d.scrollToBottom(d.prop('scrollHeight'));
+}
+
+
+//To stop functionality of mute and unmute.
+const muteUnmute = () => {
+    const enabled = myvideoStream.getAudioTracks()[0].enabled;
+    if (enabled) {
+        myvideoStream.getAudioTracks()[0].enabled = false;
+        setUnmuteButton();
+    } else {
+        setMuteButton();
+        myvideoStream.getAudioTracks()[0].enabled = true;
+    }
+}
+
+//To stop functionality of Turn on and turnoff video
+const playStop = () => {
+    console.log('object')
+    let enabled = myvideoStream.getVideoTracks()[0].enabled;
+    if (enabled) {
+        myvideoStream.getVideoTracks()[0].enabled = false;
+        setPlayVideo()
+    } else {
+        setStopVideo()
+        myvideoStream.getVideoTracks()[0].enabled = true;
+    }
+}
+
+//To show Mute and Unmute.
+const setMuteButton = () => {
+    const html = `
+      <i class="fas fa-microphone"></i>
+      <span>Mute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+const setUnmuteButton = () => {
+    const html = `
+      <i class="unmute fas fa-microphone-slash"></i>
+      <span>Unmute</span>
+    `
+    document.querySelector('.main__mute_button').innerHTML = html;
+}
+
+//To show stop video and play video
+const setStopVideo = () => {
+    const html = `
+      <i class="fas fa-video"></i>
+      <span>Stop Video</span>
+    `
+    document.querySelector('.main__video_button').innerHTML = html;
+}
+
+const setPlayVideo = () => {
+    const html = `
+    <i class="stop fas fa-video-slash"></i>
+      <span>Play Video</span>    `
+    document.querySelector('.main__video_button').innerHTML = html;
 }
